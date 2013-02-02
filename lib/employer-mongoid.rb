17f84517cbed92325fdb4ac08f8e4377d2dbcdd3
Mark Kremer <mark@without-brains.net>
2013-02-02 12:19:39 +0100

    Require Employer::Employees::AbstractEmployee before including the custom module

--- lib/employer-mongoid.rb
+++ lib/employer-mongoid.rb
@@ -2,5 +2,6 @@ require "employer-mongoid/version"
 require "employer-mongoid/pipeline"
 require "employer-mongoid/job"
 require "employer-mongoid/employee"
+require "employer/employees/abstract_employee"
 
 Employer::Employees::AbstractEmployee.send(:include, Employer::Mongoid::Employee)
